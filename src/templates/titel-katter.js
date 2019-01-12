caa3aabc6008c04431d09e5bcc873601881c87b9
John Øyvind Welle <wellej@gmail.com>
2019-01-12 18:28:28 +0100

    Now with cats...

--- src/templates/titel-katter.js
+++ src/templates/titel-katter.js
@@ -7,7 +7,36 @@ import Content, { HTMLContent } from '../components/Content'
 export const TitelKatterTemplate = ({ title, titelkatter, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
-  console.log(titelkatter)
+  const renderCats = (cats) => {
+    console.log(cats)
+    return cats.map((cat, i) => (
+      <div key={i}>
+        <p>{cat.beskrivning}</p>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url(${
+              !!cat.bild.childImageSharp
+                ? cat.bild.childImageSharp.fluid.src
+                : cat.bild
+            })`,
+          }}
+        >
+          <h2
+            className="has-text-weight-bold is-size-1"
+            style={{
+              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+              backgroundColor: '#f40',
+              color: 'white',
+              padding: '1rem',
+            }}
+          >
+            {cat.namn}
+          </h2>
+        </div>
+      </div>
+    ))
+  }
 
   return (
     <section className="section section--gradient">
@@ -19,45 +48,7 @@ export const TitelKatterTemplate = ({ title, titelkatter, content, contentCompon
                 {title}
               </h2>
               <PageContent className="content" content={content} />
-              {titelkatter.map((cat, i) => {
-                <div key={i}>
-                  <p>{cat.beskrivning}</p>
-                  <h2
-                      className="has-text-weight-bold is-size-1"
-                      style={{
-                        boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                        backgroundColor: '#f40',
-                        color: 'white',
-                        padding: '1rem',
-                      }}
-                    >
-                      {cat.namn}
-                  </h2>
-                  {/* <div
-                    className="full-width-image-container margin-top-0"
-                    style={{
-                      backgroundImage: `url(${
-                        !!cat.bild.childImageSharp
-                          ? cat.bild.childImageSharp.fluid.src
-                          : cat.bild
-                      })`,
-                    }}
-                  >
-                    <h2
-                      className="has-text-weight-bold is-size-1"
-                      style={{
-                        boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                        backgroundColor: '#f40',
-                        color: 'white',
-                        padding: '1rem',
-                      }}
-                    >
-                      {cat.namn}
-                    </h2>
-                  </div> */}
-
-                </div>
-              })}
+              {renderCats(titelkatter)}
             </div>
           </div>
         </div>
