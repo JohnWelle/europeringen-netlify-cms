22d6020f8a8ab8aaf0eba321d3b7083f7720ccbd
John Øyvind Welle <wellej@gmail.com>
2019-01-15 10:35:36 +0100

    Adds cloudinary

--- src/cms/preview-templates/CatPreview.js
+++ src/cms/preview-templates/CatPreview.js
@@ -2,9 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { CatTemplate } from '../../templates/cat'
 
-const CatPreview = ({ entry, widgetFor }) => (
+const CatPreview = ({ entry }) => (
   <CatTemplate
-    content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
@@ -14,8 +13,7 @@ const CatPreview = ({ entry, widgetFor }) => (
 CatPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
+  })
 }
 
 export default CatPreview
